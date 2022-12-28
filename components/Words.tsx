215d527885a1a9a8e2ef4258d1184db7b7e8f508
Ahmad Sorour <ahmad_sorour@outlook.com>
2022-12-28 12:17:09 +0200

    fix: now completely displaying the third word

--- components/Words.tsx
+++ components/Words.tsx
@@ -5,7 +5,7 @@ const Words = () => {
   const words = useSPGameStore((state) => state.words);
 
   return (
-    <div className="flex flex-col items-center bg-secondary p-4 rounded-lg overflow-hidden w-full h-[6.5rem]">
+    <div className="flex flex-col items-center bg-secondary p-4 rounded-lg overflow-hidden w-full h-28">
       {words.map((w) => (
         <Word word={w} key={w} />
       ))}
