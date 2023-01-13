cd68f6cc73bbc0c1773cc11f3a8df22748ce6eb9
Ahmad Sorour <ahmad_sorour@outlook.com>
2023-01-13 12:44:20 +0200

    feat: backdrop now has optional onClick event to be used to close modals

--- components/UI/Backdrop.tsx
+++ components/UI/Backdrop.tsx
@@ -1,6 +1,13 @@
-const Backdrop = () => {
+type Props = {
+  onClick?: () => void;
+};
+
+const Backdrop = (props: Props) => {
   return (
-    <div className="left-0 top-0 fixed z-10 w-screen h-screen bg-black/90"></div>
+    <div
+      onClick={props.onClick}
+      className="left-0 top-0 fixed z-10 w-screen h-screen bg-black/90"
+    ></div>
   );
 };
 export default Backdrop;
