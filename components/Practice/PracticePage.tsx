ce556d45400135f3d8c65e02286f006b6137e92f
Ahmad Sorour <ahmad_sorour@outlook.com>
2023-01-13 13:18:12 +0200

    chore: commented out conflicting component renders until implemented

--- components/Practice/PracticePage.tsx
+++ components/Practice/PracticePage.tsx
@@ -11,17 +11,17 @@ const PracticePage = () => {
   return (
     <PageWrapper>
       <div className="flex flex-col justify-center w-[90%] mx-auto gap-8 p-8 bg-main bg-opacity-60 rounded-3xl md:p-0 md:aspect-square md:w-[50%] 2xl:w-[35%]">
-        <NextWordTimer />
+        {/* Render <NextWordTimer /> specific to Practice Game Mode */}
         <GameScores />
         <div className="flex self-center flex-col items-center justify-center gap-3 md:w-[65%]">
-          <Words />
-          <WordInput />
+          {/* Render <Words /> specific to Practice Game Mode */}
+          {/* Render <WordInput /> specific to Practice Game Mode */}
         </div>
         <div className="flex justify-between gap-2 md:self-center md:w-[65%]">
-          <ToggleGameStatusButton />
-          <TimeLimitSelect />
+          {/* Render <ToggleGameStatusButton /> specific to Practice Game Mode */}
+          {/* Render <TimeLimitSelect /> specific to Practice Game Mode */}
         </div>
-        <GameOverModal />
+        {/* Render <GameOverModal /> specific to Practice Game Mode */}
       </div>
     </PageWrapper>
   );
